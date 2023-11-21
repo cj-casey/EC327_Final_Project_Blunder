aaf74dd2dbef63f87c3702b3ee7522dd31978bae
Bogdan Sadikovic <94764889+bogdansadikovic@users.noreply.github.com>
2023-11-20 21:19:15 -0500

    MongoDB Server established
    Added a key and password to the MongoDB Cloud Server. Established a document for it to call to. 
    Added a test database for it to call forth when used. 
    Added code to close server after code is run (This way we don't waste potential memory, or accidentally send incorrect data to the server.

---
+++ Mongo.java
@@ -0,0 +1,27 @@
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bson.BsonDocument;
+
+public static void main(String args[]) {
+  // Set system properties via commandline or programmatically
+  System.setProperty("javax.net.ssl.keyStore", "leaderboarddata");
+  System.setProperty("javax.net.ssl.keyStorePassword", "BogiBogi12378&*#");
+//^^^ We need to initialize a key to the server that is going to be sent. ^^^
+
+  String uri = "mongodb+srv://leaderboarddata.s28201v.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"; //URI for the Mongo Server database is established.
+  ConnectionString connectionString = new ConnectionString(uri);
+  MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(connectionString).serverApi(ServerApi.builder().version(ServerApiVersion.V1).build()).build(); //BIG DOT OPERATORS
+
+
+  MongoClient mongoClient = MongoClients.create(settings);
+  MongoDatabase database = mongoClient.getDatabase("testDB");
+  MongoCollection<Document> collection = database.getCollection("testCol");
+  BsonDocument filter = new BsonDocument();
+  collection.countDocuments(filter);
+
+  client.close(); //Closes the client server at the end so that it doesn't waste memory usage. 
+}

97f16f70f2d484437cf74d3f0b0c06341d23e793
casey <111756500+cj-casey@users.noreply.github.com>
2023-12-03 19:16:59 -0500

    checkmate test

--- test.java
+++ test.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
-
 import Piece.Piece;
 
 import java.util.HashMap;
@@ -28,15 +27,15 @@ public class test {
         Map<Character,Integer> xcoordinate = Map.of('A',0,'B',1,'C',2,'D',3,'E',4,'F',5,'G',6,'H',7);
         Map<Character,Integer> ycoordinate = Map.of('1',0,'2',1,'3',2,'4',3,'5',4,'6',5,'7',6,'8',7);
 
-        char[] rookCol = {'R', 'P', ' ', ' ', ' ', ' ', 'p', 'r'};
-        char[] rookCol2 = {'R', 'P', ' ', ' ', ' ', ' ', 'p', 'r'};
-        char[] knightCol = {'N', 'P', ' ', ' ', ' ', ' ', 'p', 'n'};
-        char[] knightCol2 = {'N', 'P', ' ', ' ', ' ', ' ', 'p', 'n'};
-        char[] bishopCol = {'B', 'P', ' ', ' ', ' ', ' ', 'p', 'b'};
-        char[] bishopCol2 = {'B', 'P', ' ', ' ', ' ', ' ', 'p', 'b'};
-        char[] queenCol = {'Q', 'P', ' ', ' ', ' ', ' ', 'p', 'q'};
-        char[] kingCol = {'K', 'P', ' ', ' ', ' ', ' ', 'p', 'k'};
-        char[][] board = {rookCol, knightCol, bishopCol, queenCol, kingCol, bishopCol2, knightCol2, rookCol2}; //fuck this thing
+        char[][] board = { //board initialization
+            {'R', 'P', ' ', ' ', ' ', ' ', 'p', 'r'},
+            {'N', 'P', ' ', ' ', ' ', ' ', 'p', 'n'},
+            {'B', 'P', ' ', ' ', ' ', ' ', 'p', 'b'},
+            {'Q', 'P', ' ', ' ', ' ', ' ', 'p', 'q'},
+            {'K', 'P', ' ', ' ', ' ', ' ', ' ', 'k'},
+            {'B', ' ', 'P', ' ', ' ', ' ', 'p', 'b'},
+            {'N', 'P', ' ', 'P', ' ', ' ', 'p', 'n'},
+            {'R', 'P', ' ', ' ', ' ', ' ', 'p', 'r'}};
 
         List<Piece> whitePieces = new ArrayList<>();
         List<Piece> blackPieces = new ArrayList<>();
@@ -54,19 +53,28 @@ public class test {
                 }
                 }
             }
-           /* System.out.println("Black Pieces");
+
+         /*   System.out.println("Black Pieces");
              for(int i = 0; i < blackPieces.size(); i++) {
                 System.out.println(blackPieces.get(i));
+            } 
+            System.out.println("White Pieces");
+             for(int i = 0; i < whitePieces.size(); i++) {
+                System.out.println(whitePieces.get(i));
             } */
        // String userInput = null;
         //System.out.println("Enter in the coordinates of the Piece you want to move in the format A1:");
 
        
-        drawBoard(board);
-
+        Piece queen = new Piece(board,3,7);
         Piece test = new Piece(board,4,0);
-        test.handleMovement(board,5,1,blackPieces);
-        drawBoard(board);
-       
+        Piece test2 = new Piece(board,0,0);
+        System.out.println(test.stalemateOrCheckmate(board, whitePieces, blackPieces));
+        queen.handleMovement(board,7,3,whitePieces,blackPieces);
+        System.out.println(test.stalemateOrCheckmate(board, whitePieces, blackPieces));
+
+            
+    
+        
     }
 }
